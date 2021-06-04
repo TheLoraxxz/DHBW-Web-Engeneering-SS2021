3ca0ed6bcf5eda27690c6d6cf8ec823f8f5d4d99
TheLoraxxz <tilmannn.lorenz@mail.com>
2021-06-04 20:53:12 +0200

    Weiterer Ausbau von Webseite home (muss noch die home seite ordentlich gestalten auf papier :D)

--- scripts/home.php
+++ scripts/home.php
@@ -1,8 +1,9 @@
 <?php
 ?>
 <div>
-    <video controls autoplay>
-        <source src="../assets/videos/World%20of%20warcraft%20video.mp4" type="video/mp4">
+    <video controls autoplay playsinline="playsinline">
+        <source src="../assets/videos/WoW_Trailer/WoW_background_vid.mp4" type="video/mp4">
+        <source src="../assets/videos/" type="video/webm">
     </video>
     <p>Kaufen</p>
     <ul>
@@ -14,6 +15,7 @@
         <li>World of Warcraft</li>
         <li>The Elder Scrolls V</li>
     </ul>
+    <h1>Worum geht es?</h1>
     <p>Seit dem 1952 OXO, das erste Computerspiel (ein TicTacToe Spiel)
         mit einem bekannten Namen, erschienen ist, sind tausende von Spielen erschienen.
     Heutzutage erscheinen im stündlichen Takt neue Videospiele mit manchmal komischen Ideen.
@@ -22,4 +24,26 @@
     ihnen unsere Lieblingsspiele zu zeigen. Fabian hat über sein Lieblingsspiel <strong>Skyrim</strong> geschrieben
     während Lukas über sein Lieblingsspiel <strong>World of Warcraft</strong> berichtet hat. Klicke oben einfach
     auf die jeweiligen Icons um etwas über das jeweilige Spiel zu erfahren</p>
+    <h1>Lerne das Entwicklerteam kennen</h1>
+    <div class="devs">
+        <h2>Tilmann Lorenz</h2>
+        <p>Tilmann Lorenz ist ein Softwareentwickler. er hat schon viele Projekte begleitet
+            wie die App der DHBW Mosbach/ Bad Merengtenheim von der
+            Studenten Vertretung. Auch programmierte er in einem 2 Monatigen Praktikum
+            bei einer Softwarefirma in Dresden das Intranet. Ich weiß nicht was
+            ich hier noch schreiben soll. Ich muss allerdings ein wenig Platz füllen :D.
+            Er spielt am liebsten Overwatch. Ein äußerst interessantes Spiel, dass durch
+            die vielen verschiedenen Helden mit unterschiedlichen Fähigkeiten im
+            Shooterbereich bekannt ist.
+            <br>Er ist für den groben Aufbau der Webseite zuständig. Des
+        Weiteren auch für die Homepage die Sie gerade ansehen.</p>
+    </div>
+    <div>
+        <h2>Fabian Zink</h2>
+        <p><em>blablablbal</em></p>
+    </div>
+    <div>
+        <h2>Lukas S.</h2>
+        <p><em>blablablbal</em></p>
+    </div>
 </div>
