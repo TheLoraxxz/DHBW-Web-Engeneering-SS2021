0f92b8ef60b0e200e0dad9d29befc8968066d2cb
Lukas Weber <lukas.weber@mail.schwarz>
2021-07-16 16:44:58 +0200

    wow-teil beifügen

---
+++ scripts/wow.php
@@ -0,0 +1,126 @@
+ <div class="site_background">
+ <div class="header">
+    <img class="header_wallpaper" src="./assets/img/wow_wallpaper.jpg" />
+  </div>
+  <div class="wow_description">
+    World of Warcraft, abgekürzt WoW, ist ein über das Internet spielbares Computer-Rollenspiel (MMORPG) des Spielentwicklers
+    Blizzard Entertainment. Es wurde 2004 erstmals veröffentlicht, in Europa erschien es 2005, später auch in anderen Ländern, unter anderem in der Volksrepublik China.
+    World of Warcraft erhielt 2009 den Guinness-Weltrekord für das beliebteste Multiplayer-Online-Rollenspiel. Bis Juli 2012 generierte das Spiel über zehn Milliarden US-Dollar Umsatz und machte es damit zum erfolgreichsten Computerspiel.[2] Um World of Warcraft spielen zu können, muss ein monatliches Entgelt entrichtet werden. Daneben gibt es eine kostenlose, mit Einschränkungen verbundene „Starter Edition“.
+    Seit dem Erscheinen der Grundversion wurden die Erweiterungen The Burning Crusade (2007), Wrath of the Lich King (2008), Cataclysm (2010), Mists of Pandaria (2012), Warlords of Draenor (2014), Legion (2016), Battle for Azeroth (2018) und Shadowlands (2020) veröffentlicht. 2019 erschien eine Wiederveröffentlichung der Grundversion und 2021 eine Wiederveröffentlichung von The Burning Crusade.
+    <details class="blizard_details">
+      <summary>Blizzard Entertainment</summary>
+      <p>amerikanisches Entwicklerstudio gegründet 1991 mit einem Umsatz von 1, 8 milliarden Dollar im Jahr 2020</br>
+      Weitere Produkte des Unternehmens sind Spiele wie Overwatch, Heartstone oder die Diablo Reihe, Heroes of the Storm oder Starcraft.</br>
+      Fusion mit dem Spieleentwickler Activision und Gründung des Konzerns Activision Blizzard.</br>
+      Wert des Konzerns:    62 milliarden USD
+    </p>
+    </details>
+  </div>
+  <div class="wow_charakterauswahl">
+    <h2><b>Charakterauswahl</b></h2>
+    <p>
+      Zu Beginn des Spiels, muss der Spieler einen individualisierten Charakter erstellen. Hierbei gibt es unzählige Anpassungsmöglichkeiten.
+      So muss zunächst aus einer der 13 spielbaren Rassen ausgewählt werden:
+    </p>
+    <div class="races">
+      <div class="races_table_cont">
+      <table class="race_list">
+        <caption><h3>Rassen in Azeroth</h3></caption>
+        <thead>
+            <tr>
+              <th><h4>Allianz</h4></th>
+              <th><h4>Horde</h4></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="race_tableitem">
+              <td>Menschen</td>
+              <td>Orcs</td>
+            </tr>
+            <tr class="race_tableitem">
+              <td>Nachtelfen</td>
+              <td>Blutelfen</td>
+            </tr>
+            <tr class="race_tableitem">
+              <td>Drenai</td>
+              <td>Untote</td>
+            </tr>
+            <tr class="race_tableitem">
+              <td>Zwerge</td>
+              <td>Tauren</td>
+            </tr>
+            <tr class="race_tableitem">
+              <td>Gnome</td>
+              <td>Trolle</td>
+            </tr>
+            <tr class="race_tableitem">
+              <td>Worgen</td>
+              <td>Goblins</td>
+            </tr>
+            <tr class="race_tableitem">
+              <td colspan="2">Pandaren</td>
+            </tr>
+        </tbody>
+    </table>
+  </div>
+    <div class="races_picture">
+      <img src="./assets/img/races.jpg" width="90%">
+    </div>
+  </div>
+    <p>
+    Die genanten Rassen organisieren sich gemeinsam in zwei rivalisierenden Fraktionen. So gehören die Nachtelfen zusammen mit den Zwergen, Dreanai, Gnomen und Worgen zu der von den Menschen 
+    angeführten Allianz. Die Blutelfen, Orcs, Tauren, Trolle, Untote und Goblins bilden gemeinsam die sogenante Horde. Die einzige Außnahme in dieser Struktur bilden die Pandaren. Spieler die diese Rasse wählen, müssen später im Spielgeschehen 
+    entscheiden, welcher der beiden Fraktionen sie sich anschließen möchten.
+    </p>
+    <p>
+      Nachdem diese Wahl getroffen ist, kann der Charakter individualisiert werden. Hierbei gibt es je nach Rasse eine Auswahl zwischen verschiedenen Merkmalen. 
+      Beispielsweise kann die Gesichtsform, die Frisur oder die Hautfarbe ausgewählt werden. Aber auch rassenspezifische Eigenschaften wie z.B Hörner bei den Tauren oder die Ohren der Elfen bieten mehrere Auswahlmöglichkeiten.
+    </p>
+  </div>
+  <div class="wow_klassenwahl">
+    <h2><b>Klassenwahl</b></h2>
+    <p>Sobald der Charakter erstellt wurde, muss der Spieler entscheiden in welche Klasse er während des Spiels schlüpfen möchte. Hier gibt es insgesamt zwölf verschiedene Klasse, welche sich in ihren Fähigkeiten unterscheiden.</p>
+    <ol class="classes_list">
+      <li class="classes_listitem">Krieger</li>
+      <li class="classes_listitem">Paladin</li>
+      <li class="classes_listitem">Jäger</li>
+      <li class="classes_listitem">Schamane</li>
+      <li class="classes_listitem">Druide</li>
+      <li class="classes_listitem">Todesritter</li>
+      <li class="classes_listitem">Magier</li>
+      <li class="classes_listitem">Hexenmeister</li>
+      <li class="classes_listitem">Priester</li>
+      <li class="classes_listitem">Mönch</li>
+      <li class="classes_listitem">Dämonenjäger</li>
+      <li class="classes_listitem">Schurke</li>
+    </ol>
+    <p>
+      Die verschiedenen Klassen werden nochmals in unterschiedliche Rollen aufgeteilt. So gibt es sogenannte Tanks, Heiler und DDs. 
+      Ein "Tank" ist ein Charakter mit starker Rüstung, welcher die Gegner im Kampf an sich bindet und verhindert, dass sie seine Gruppenmitglieder angreifen. Der Heiler hingegen, stellt mit Hilfe verschiedener 
+      Zaubersprüche die durch Angriffe verlorenen Lebenspunkte wieder her, oder schützt seine Gruppenmitglieder mit schadensabsorbierenden Schildern. Die Damage Dealer (DDs) sind die Schadensausteiler. Hierbei wird zwischen Fernkämpfern und Nahkämpfern unterschieden. Die Fernkämpfer sind 
+      in der Regel Charaktere welche Zauber wirken (Magier, Hexenmeister). Die Nahkämpfer suchen hingegen die direkte Konfrontation mit dem Gegner.
+    </p>
+  </div>
+  <div class="wow_spielprinzip">
+  <h2><b>Spielprinzip</b></h2>
+    <p>
+      Ist der Charakter ersteinmal erstellt, kann das Abenteuer in der weiten Welt von Azeroth beginnen. Das Ziel des Spiels ist es, den Charakter immer weiter zu entwickeln. Dazu können Aufgaben, sogenannte Quest erfüllt werden
+      um Erfahrungspunkte zu sammeln. Hat man genug Erfahrungspunkte gesammelt steigt man ein Level auf. Dieser Prozess wiederholt sich, bis die maximale Stufe erreicht ist. Sobald dieses Ziel abgeschlossen wurde, kann in den "End-Content"
+      eingestiegen werden. Hier geht das Spiel erst richtig los. Viele Spieler organisieren sich hierzu in Gilden, vergleichbar mit Vereinen in den wahren Welt. Sofern einige willige Mitstreiter gefunden sind, kann gemeinsam mit einer Gruppe aus anderen Spielern versucht werden schwierige Herausforderungen zu meistern. Als Belohnung kann dabei
+      neue und vorallem hochwertigere Ausrüstung erhalten werden. Dadurch wird der Charakter des Spielers immer stärker und kann auch immer schwierigere Aufgaben meißtern. 
+    </p>
+    </div>
+    <div class="esport">
+    <h2><b>E-Sports</b></h2>
+      <p>
+        Die größte und bekannteste dieser oben erwähnten Herausforderungen ist der sogenannte Raid. Eine Gruppe von 20 Spielern versucht zusammen extrem starke Gegner zu bezwingen. Die neue Eröffnung eines Raids
+        in World of Warcraft ist ein großes Event. Auch in Hinsicht auf den E-Sport. Viele Organisationen beteiligen sich an dem daraus hervorgehenden "Race to World First". Die besten Gilden versuchen
+        den Endgegner als erstes zu bezwingen. Dieses "Rennen" kann sich über mehrere Wochen hinweg ziehen und die Fans können den Vortschritt der jeweiligen Gruppen in diversen Liveübertragungen nachverfolgen.
+        Im nachfolgenden Video ist die Gilde "Limit" zu sehen, welche den Endgegner "Sire Denathrius" im aktuellen Raid "Schloss Nathria" weltweit zuerst bezwingt. Auch ist die dazu notwendige Koordinierung der
+        Gruppe zu sehen. Jeder der zwanzig Spieler hat eine genau definierte Rolle und die erdachte Taktik zum besiegen des Gegners muss genauestens ausgeführt werden. 
+      </p>
+      <div class="esports_video">
+        <iframe src="https://www.youtube.com/embed/19tXeHJns1E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> 
+      </div>
+      </div>
+</div>
