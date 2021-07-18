2992095f8542040ca5e37b862e60381ced241696
the_loraxxz <the_loraxxz@web.de>
2021-07-18 16:02:08 +0200

    Added Audio /abbr

--- scripts/home.php
+++ scripts/home.php
@@ -43,7 +43,7 @@
             Manchmal sind es besondere Ideen und manchmal ist es einfach nur ein spiel um schnell Gewinn
             zu machen. Egal je doch welches Spiel.<br> Diese neuartig kreeirte Webseite ist dazu da
             ihnen unsere Lieblingsspiele zu zeigen. Fabian hat über sein Lieblingsspiel <strong>Skyrim</strong> geschrieben
-            während Lukas über sein Lieblingsspiel <strong>World of Warcraft</strong> berichtet hat. Klicke oben einfach
+            während Lukas über sein Lieblingsspiel <strong><abbr title="World of Warcraft">WoW</abbr> </strong> berichtet hat. Klicke oben einfach
             auf die jeweiligen Icons um etwas über das jeweilige Spiel zu erfahren</p>
         <figure>
             <img src="./assets/img/OXO.jpg" alt="OXO, eines der ersten Programmierspiele">
